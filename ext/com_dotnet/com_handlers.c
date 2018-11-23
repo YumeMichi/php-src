86b151cc5b88b36f97e4d79bc3fc90665b578e0c
Christoph M. Becker <cmbecker69@gmx.de>
2018-11-23 16:32:33 +0100

    Fix #77177: Serializing or unserializing COM objects crashes
    Firstly, we avoid returning NULL from the get_property handler, but
    instead return an empty HashTable, which already prevents the crashes.
    Secondly, since (de-)serialization obviously makes no sense for COM,
    DOTNET and VARIANT objects (at least with the current implementation),
    we prohibit it right away.
    
    (cherry picked from commit 115ee49b0be12e3df7d2c7027609fbe1a1297e42)

--- ext/com_dotnet/com_handlers.c
+++ ext/com_dotnet/com_handlers.c
@@ -29,6 +29,8 @@
 #include "php_com_dotnet_internal.h"
 #include "Zend/zend_exceptions.h"
 
+const HashTable com_dotnet_object_properties;
+
 static zval *com_property_read(zval *object, zval *member, int type, const zend_literal *key TSRMLS_DC)
 {
 	zval *return_value;
@@ -239,7 +241,7 @@ static HashTable *com_properties_get(zval *object TSRMLS_DC)
 	 * infinite recursion when the hash is displayed via var_dump().
 	 * Perhaps it is best to leave it un-implemented.
 	 */
-	return NULL;
+	return &com_dotnet_object_properties;
 }
 
 static void function_dtor(void *pDest)
